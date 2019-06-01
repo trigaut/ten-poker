889fbb235e00f16860115081bda75d2ce3518447
therewillbecode <tomw08@gmail.com>
2019-06-01 10:19:38 +0100

    Handle Maybe Cards -> Null on Front end

--- client/app/components/Game.js
+++ client/app/components/Game.js
@@ -7,58 +7,80 @@ import Card from './Card'
 
 import { fromJS } from 'immutable'
 
-const getSeatedPlayer = (username, player, gameStage, position, isTurnToAct) =>
+const getSeatedPlayer = (
+  username,
+  player,
+  gameStage,
+  position,
+  isTurnToAct
+) => (
   <Seat
     key={position}
     position={position}
     playerName={player.get('_playerName')}
     chips={player.get('_chips')}
-    hasPocketCards={player.get('_playerName') !== username && player.get('_playerState') === 'In' && gameStage !== 'PreDeal' && gameStage !== 'Showdown'}
+    hasPocketCards={
+      player.get('_playerName') !== username &&
+      player.get('_playerState') === 'In' &&
+      gameStage !== 'PreDeal' &&
+      gameStage !== 'Showdown'
+    }
     isTurnToAct={isTurnToAct && gameStage !== 'Showdown'}
   />
+)
 
 const getSeats = (username, maxPlayers, players, gameStage, currentPosToAct) =>
-  Array(maxPlayers).fill(null).map((_, i) => {
-    const player = players.get(i)
-    const isTurnToAct = i === currentPosToAct
+  Array(maxPlayers)
+    .fill(null)
+    .map((_, i) => {
+      const player = players.get(i)
+      const isTurnToAct = i === currentPosToAct
 
-    return player ? getSeatedPlayer(username, player, gameStage, i, isTurnToAct) :
-      <Seat
-        key={i}
-        position={i}
-      />
+      return player ? (
+        getSeatedPlayer(username, player, gameStage, i, isTurnToAct)
+      ) : (
+        <Seat key={i} position={i} />
+      )
+    })
+
+const getPocketCards = players =>
+  players.map((p, i) => {
+    if (p.get('_pockets') === null) {
+      return ''
+    }
+
+    return p.get('_pockets').length !== 0 ? (
+      <div className={`showdown-pocket-cards-${i}`} key={p.get('_playerName')}>
+        <div className="showdown-pocket-cards-container">
+          {p.get('_pockets').map(card => {
+            const rank = card.get('rank')
+            const suit = card.get('suit')
+
+            return <Card key={rank + suit} rank={rank} suit={suit} />
+          })}
+        </div>
+      </div>
+    ) : (
+      ''
+    )
   })
 
-const getPocketCards = players => players.map((p, i) =>
-  p.get('_pockets').length !== 0 ?
-    (<div className={`showdown-pocket-cards-${i}`} key={p.get('_playerName')} >
-      <div className='showdown-pocket-cards-container' >
-        {p.get('_pockets').map(card => {
-          const rank = card.get('rank')
-          const suit = card.get('suit')
-
-          return (<Card
-            key={rank + suit}
-            rank={rank}
-            suit={suit}
-          />)
-        })}
-      </div>
-    </div>) : ''
-)
-
-const getPlayerBets = players => players.map((p, i) => p.get('_bet') > 0 ?
-  <div className={`player-bet-container-pos-${i}`}>
-    <div className={`player-bet-pos-${i}`}>
-      <div className='player-bet-chip'></div>
-      <div className='player-bet-label'>
-        <span className='monospaced-font'>
-          {`$${p.get('_bet')}`}
-        </span>
-      </div>
-    </div>
-  </div> : ''
-)
+const getPlayerBets = players =>
+  players.map(
+    (p, i) =>
+      p.get('_bet') > 0 ? (
+        <div className={`player-bet-container-pos-${i}`}>
+          <div className={`player-bet-pos-${i}`}>
+            <div className="player-bet-chip" />
+            <div className="player-bet-label">
+              <span className="monospaced-font">{`$${p.get('_bet')}`}</span>
+            </div>
+          </div>
+        </div>
+      ) : (
+        ''
+      )
+  )
 
 const Game = props => {
   const { game, username, isTurnToAct } = props
@@ -71,50 +93,84 @@ const Game = props => {
     const maxPlayers = 6
     const gameStage = game.get('_street')
     const potSize = game.get('_pot')
-    const userPlayer = game.get('_players').find(p => p.get('_playerName') === username)
+    const userPlayer = game
+      .get('_players')
+      .find(p => p.get('_playerName') === username)
     const userPocketCards = userPlayer ? userPlayer.get('_pockets') : null
     const currentPosToAct = game.get('_currentPosToAct')
-    const isMultiplayerShowdown = game.get('_winners').get('tag') == 'MultiPlayerShowdown'
+    const isMultiplayerShowdown =
+      game.get('_winners').get('tag') == 'MultiPlayerShowdown'
     const showdownPots = game.get('_winners').get('contents')
-    const mainShowdownPot = showdownPots ? showdownPots.get ? showdownPots.get(0) : null : null
-    const mainShowdownPotHandRanking = mainShowdownPot ? mainShowdownPot.get(0).get(0) : null
-    const mainShowdownPotHandCards = mainShowdownPot ? mainShowdownPot.get(0).get(1) : null
-    const mainShowdownPotHandPlayers = mainShowdownPot ? mainShowdownPot.get(1) : null
-    const playerNamesWinnersOfMainShowdownPot = mainShowdownPot ? mainShowdownPot.get(1) : null
+    const mainShowdownPot = showdownPots
+      ? showdownPots.get
+        ? showdownPots.get(0)
+        : null
+      : null
+    const mainShowdownPotHandRanking = mainShowdownPot
+      ? mainShowdownPot.get(0).get(0)
+      : null
+    const mainShowdownPotHandCards = mainShowdownPot
+      ? mainShowdownPot.get(0).get(1)
+      : null
+    const mainShowdownPotHandPlayers = mainShowdownPot
+      ? mainShowdownPot.get(1)
+      : null
+    const playerNamesWinnersOfMainShowdownPot = mainShowdownPot
+      ? mainShowdownPot.get(1)
+      : null
 
-    return (<div className='game-view-grid'>
-
-      <div className='game-container'>
-        < p style={{ height: '300px', top: '80px', position: 'absolute' }
-        }>
-          {(JSON.stringify({ ...jsgame, isTurnToAct, username }, undefined, '\n'))
-          }
-        </p >
-        <div className='table-container'>
-          {getPocketCards(players)}
-          {getSeats(username, maxPlayers, players, gameStage, currentPosToAct)}
-          <div className='game-grid'>
-            {players ? players.count() > 1 ?
-              <div className={`dealer-btn-pos-${dealerPos}`}>
-              </div>
-              : '' : ''}
-            <Board cards={game.get('_board')} />
-            {getPlayerBets(players)}
-            <h4 className='pot-label'>
-              <span className='monospaced-font'>
-                {`$${potSize}`}
-              </span>
-            </h4>
-            {mainShowdownPot ? <p className='winners-label'>
-              {`${mainShowdownPotHandPlayers} wins with ${mainShowdownPotHandRanking}`}
-            </p> : ''}
-          </div>
-          <div className='game-table'>
+    return (
+      <div className="game-view-grid">
+        <div className="game-container">
+          <p style={{ height: '300px', top: '80px', position: 'absolute' }}>
+            {JSON.stringify(
+              { ...jsgame, isTurnToAct, username },
+              undefined,
+              '\n'
+            )}
+          </p>
+          <div className="table-container">
+            {getPocketCards(players)}
+            {getSeats(
+              username,
+              maxPlayers,
+              players,
+              gameStage,
+              currentPosToAct
+            )}
+            <div className="game-grid">
+              {players ? (
+                players.count() > 1 ? (
+                  <div className={`dealer-btn-pos-${dealerPos}`} />
+                ) : (
+                  ''
+                )
+              ) : (
+                ''
+              )}
+              <Board cards={game.get('_board')} />
+              {getPlayerBets(players)}
+              <h4 className="pot-label">
+                <span className="monospaced-font">{`$${potSize}`}</span>
+              </h4>
+              {mainShowdownPot ? (
+                <p className="winners-label">
+                  {`${mainShowdownPotHandPlayers} wins with ${mainShowdownPotHandRanking}`}
+                </p>
+              ) : (
+                ''
+              )}
+            </div>
+            <div className="game-table" />
           </div>
         </div>
+        <ActionPanel
+          {...props}
+          gameStage={gameStage}
+          userPocketCards={userPocketCards}
+        />
       </div>
-      <ActionPanel {...props} gameStage={gameStage} userPocketCards={userPocketCards} />
-    </div>)
+    )
   }
   return <h2>no Game State</h2>
 }
