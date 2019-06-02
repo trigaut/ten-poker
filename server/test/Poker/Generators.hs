aeef675b6066b3507b3d7bc33242f628917eb9e4
therewillbecode <tomw08@gmail.com>
2019-06-02 10:50:13 +0100

    Add Player Generator

--- server/test/Poker/Generators.hs
+++ server/test/Poker/Generators.hs
@@ -14,8 +14,7 @@ import           Control.Monad
 
 import qualified Data.List as List
 import qualified Data.Text as Text
-import Data.Singletons
-import Data.Singletons.TH 
+
 import Data.Proxy
 import Data.Maybe
 
@@ -28,7 +27,10 @@ import qualified Data.Vector as V
 import Debug.Trace
 
 import Prelude 
+import Data.Text (Text)
+import qualified Data.Text as T
 import Data.Either
+
 
 import Poker.ActionValidation
 import Poker.Game.Blinds
@@ -39,7 +41,14 @@ import Poker.Types
 import Poker.Game.Hands
 
 genShuffledCards :: Int -> Gen [Card]
-genShuffledCards n = Gen.shuffle (unDeck initialDeck) >>= \cs -> return $ take n cs
+genShuffledCards n = do 
+     cs <- Gen.shuffle $ unDeck initialDeck
+     return $ take n cs
+
+genShuffledDeck :: Gen Deck
+genShuffledDeck = do 
+    cs <- Gen.shuffle $ unDeck initialDeck 
+    return $ Deck cs
 
 genSuit :: Gen Suit
 genSuit = Gen.enum Diamonds Spades
@@ -49,3 +58,33 @@ genSameSuitCards n = do
      suit' <- genSuit
      cs <- Gen.shuffle $ filter ((== suit') . suit) (unDeck initialDeck)
      return $ take n cs
+
+genPockets :: Gen (Maybe PocketCards)
+genPockets = do
+     Deck cards <- genShuffledDeck
+     let [c1, c2] = take 2 cards
+     return $ Just $ PocketCards c1 c2
+
+genNoPockets :: Gen (Maybe PocketCards)
+genNoPockets = return Nothing
+
+genPlayer :: [PlayerState] -> Int -> Gen Player
+genPlayer possibleStates position = do
+  _playerState <- Gen.element possibleStates
+  _actedThisTurn <- Gen.bool
+  _chips <- Gen.int $ Range.linear 0 10000
+  _committed <- Gen.int $ Range.linear 0 10000
+  _bet <- Gen.int $ Range.linear 0 _committed
+  _actedThisTurn <- Gen.bool
+  _pockets <- genPockets
+  return Player {..}
+    where _playerName = "player" <> (T.pack $ show position) 
+
+--    data Player = Player
+--  { _pockets :: Maybe PocketCards
+--  , _chips :: Int
+--  , _bet :: Bet
+--  , _playerState :: PlayerState
+--  , _playerName :: Text
+--  , _committed :: Bet
+--  , _actedThisTurn :: Bool
