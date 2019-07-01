01c85a89e4384444e4037b7ab9337e779c53c415
therewillbecode <tomw08@gmail.com>
2019-07-01 13:44:39 +0000

    Modified Stack GHC Version For IDE-Engine Interop

--- server/app/Main.hs
+++ server/app/Main.hs
@@ -2,31 +2,31 @@
 
 module Main where
 
-import Control.Concurrent.Async
-import Database.Redis (defaultConnectInfo)
-import Network.Wai.Handler.Warp (run)
-import Prelude
-import qualified System.Remote.Monitoring as EKG
-import Data.Text.Encoding as TSE
-import qualified Data.ByteString.Lazy as BL
+import           Control.Concurrent.Async
+import           Database.Redis                 ( defaultConnectInfo )
+import           Network.Wai.Handler.Warp       ( run )
+import           Prelude
+import qualified System.Remote.Monitoring      as EKG
+import           Data.Text.Encoding            as TSE
+import qualified Data.ByteString.Lazy          as BL
 
-import qualified Data.ByteString.Lazy.Char8 as C
+import qualified Data.ByteString.Lazy.Char8    as C
 
-import API
-import Database
-import Env
-import Socket
+import           API
+import           Database
+import           Env
+import           Socket
 
-import Data.Proxy
-import Types
-import Crypto.JWT
+import           Data.Proxy
+import           Types
+import           Crypto.JWT
 
 main :: IO ((), ())
 main = do
-  dbConnString <- getDBConnStrFromEnv
-  userAPIPort <- getAuthAPIPort defaultUserAPIPort
+  dbConnString  <- getDBConnStrFromEnv
+  userAPIPort   <- getAuthAPIPort defaultUserAPIPort
   socketAPIPort <- getSocketAPIPort defaultSocketAPIPort
-  redisConfig <- getRedisHostFromEnv defaultRedisHost
+  redisConfig   <- getRedisHostFromEnv defaultRedisHost
   print "REDIS config: "
   print redisConfig
   secretKey <- getSecretKey
@@ -36,11 +36,11 @@ main = do
   migrateDB dbConnString
   ekg <- runMonitoringServer
   concurrently runUserAPI runSocketAPI
-  where
-    defaultUserAPIPort = 8000
-    defaultSocketAPIPort = 5000
-    defaultRedisHost = "localhost"
-    defaultMonitoringServerAddress = "localhost"
-    defaultMonitoringServerPort = 9999
-    runMonitoringServer =
-      EKG.forkServer defaultMonitoringServerAddress defaultMonitoringServerPort
+ where
+  defaultUserAPIPort             = 8000
+  defaultSocketAPIPort           = 5000
+  defaultRedisHost               = "localhost"
+  defaultMonitoringServerAddress = "localhost"
+  defaultMonitoringServerPort    = 9999
+  runMonitoringServer =
+    EKG.forkServer defaultMonitoringServerAddress defaultMonitoringServerPort
