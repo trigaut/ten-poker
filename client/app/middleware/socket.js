4b7d21f027213aa13c0eeb0613c1fdc6ce516bcd
Tom Chambrier <tomw08@gmail.com>
2019-11-14 12:04:21 +0000

    Set API Urls Based on NODE_ENV Env Var

--- client/app/middleware/socket.js
+++ client/app/middleware/socket.js
@@ -12,10 +12,7 @@ import { newGameState } from '../actions/games'
 
 import * as types from '../actions/types'
 
-const SOCKET_API_URL =
-  process.env.SOCKET_API_URL === 'production'
-    ? process.env.SOCKET_API_URL || 'ws://209.97.176.231:5000'
-    : process.env.SOCKET_API_URL || 'ws://localhost:5000'
+const SOCKET_API_URL = process.env.NODE_ENV === 'production' ? 'ws://209.97.176.231:5000' : 'ws://localhost:5000'
 
 function addHandlers(socket, authToken, dispatch) {
   socket.onopen = event => {
