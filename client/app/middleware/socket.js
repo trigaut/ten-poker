cb847f02173a1f11f6369d4b33d0f974d1865f12
Tom Chambrier <tomw08@gmail.com>
2019-11-15 13:27:11 +0000

    Use Secure Websocket Endpoint on Client

--- client/app/middleware/socket.js
+++ client/app/middleware/socket.js
@@ -12,7 +12,7 @@ import { newGameState } from '../actions/games'
 
 import * as types from '../actions/types'
 
-const SOCKET_API_URL = process.env.NODE_ENV === 'production' ? 'ws://tenpoker.co.uk:5000' : 'ws://localhost:5000'
+const SOCKET_API_URL = process.env.NODE_ENV === 'production' ? 'wss://tenpoker.co.uk:5000' : 'ws://localhost:5000'
 
 function addHandlers(socket, authToken, dispatch) {
   socket.onopen = event => {
